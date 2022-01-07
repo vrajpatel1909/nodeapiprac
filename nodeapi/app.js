f928ca21bf80504f5986c3d177b331b2d779f316
Vraj Patel <68704053+vrajpatel1909@users.noreply.github.com>
2022-01-07 15:05:11 +0530

    Add files via upload

---
+++ nodeapi/app.js
@@ -0,0 +1,19 @@
+ const express = require('express')
+ const mongoose = require('mongoose')
+ const url = 'mongodb://localhost/AlienDBex'
+ const app = express()
+
+ mongoose.connect(url, {useNewUrlParser:true})
+ const con = mongoose.connection
+
+ con.on('open', () => {
+     console.log('connnected..')
+ })
+
+app.use(express.json( ))
+
+ const alienRouter = require('./routers/aliens')
+ app.use('/aliens',alienRouter)
+ app.listen(9000, () => {
+     console.log('Server started')
+ })
